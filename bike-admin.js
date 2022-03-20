2f472b779859f72c35478a8fd7a9d1600252ddfd
Nell B <nellb28@yahoo.com>
2022-03-20 08:35:14 -0500

    Renaming variables

--- bike-admin.js
+++ bike-admin.js
@@ -1,116 +1,114 @@
-// const formEl = document.getElementById("best-bikes-form");
-// const yearEl = document.getElementById("year");
-// const monthEl = document.getElementById("month");
-// const dateEl = document.getElementById("date");
+const BASE_URI = "http://api.citybik.es";
 
-// formEl.addEventListener("submit", function (e) {
-//   e.preventDefault();
+fetchNetworkDetails().then((network) => {
+  console.log(network); // fetched stations
+  generateNetworkDetailTable(network);
+});
 
-//   const year = yearEl.value;
-//   const month = monthEl.value;
-//   const date = dateEl.value;
-
-//   url = buildURL(year, month, date);
-//   getbikeData(url);
-// });
-const results = getbikeData();
-
-async function getbikeData() {
-  url = "http://api.citybik.es/v2/networks";
-  const response = await fetch(url)
-    .then(function (data) {
-      return data.json();
-    })
-    .then(function (responseJson) {
-      console.log(responseJson);
-      generateBikeTable(responseJson);
-    })
-    .catch(() => {
-      console.log("nothing from fetch");
-      //console.log(responseJson);
-      // generateTableHeader();
-    });
+async function fetchNetworks() {
+  const response = await fetch(`${BASE_URI}/v2/networks`);
+  const networks = await response.json();
+  return networks;
 }
 
-function buildURL(year, month, date) {
-  let DATE = year + "-" + month + "-" + date;
+async function fetchNetworkDetails() {
+  const response = await fetch(`${BASE_URI}/v2/networks/divvy`);
+  const network = await response.json();
+  return network;
+}
 
-  if (!year || !month || !date) {
-    DATE = "current";
+function generateNetworkDetailTableHeader() {
+  const networkContainer = document.getElementById("networks-container");
+
+  if (document.getElementById("network-table")) {
+    networkContainer.removeChild(
+      document.getElementById("network-section-table")
+    );
   }
-  // create api-key.js file with const API_KEY="your_api_key" in this same directory to use
-  const BASE_URL = `https://api.nytimes.com/svc/bikes/v3/lists/`;
-  const ENDPONT = `${DATE}/hardcover-fiction.json`;
-  const URL = `${BASE_URL}${ENDPONT}?api-key=${API_KEY}`;
-  console.log(URL);
-  return URL;
+  const networkDetailTable = document.createElement("table");
+  networkDetailTable.setAttribute("id", "network-table");
+
+  networkContainer.appendChild(networkDetailTable);
+  const networkDetailRow = document.createElement("tr");
+  networkDetailTable.appendChild(networkDetailRow);
+  const networkHeader1 = document.createElement("th");
+  networkDetailTable.appendChild(networkHeader1);
+  networkHeader1.innerHTML = " Company";
+  const networkHeader2 = document.createElement("th");
+  networkDetailTable.appendChild(networkHeader2);
+  networkHeader2.innerHTML = " Country";
+  const networkHeader3 = document.createElement("th");
+  networkDetailTable.appendChild(networkHeader3);
+  networkHeader3.innerHTML = " City";
+
+  return networkDetailTable;
 }
-
-// //TODO add logic to provide a message when fetch returns a non 200 response
-// function getbikeData(url) {
-//   fetch(url)
-//     .then(function (data) {
-//       return data.json();
-//     })
-//     .then(function (responseJson) {
-//       console.log(responseJson);
-
-//       //      generatebikeTable(responseJson);
-//     })
-//     .catch(() => {
-//       //      generateTableHeader();
-//     });
-// }
 
 function generateTableHeader() {
-  const bikeContainer = document.getElementById("bikes-container");
+  const networkContainer = document.getElementById("networks-container");
 
-  if (document.getElementById("bike-table")) {
-    bikeContainer.removeChild(document.getElementById("bike-table"));
+  if (document.getElementById("network-table")) {
+    networkContainer.removeChild(document.getElementById("network-table"));
   }
-  const bikeTable = document.createElement("table");
-  bikeTable.setAttribute("id", "bike-table");
-  bikeContainer.appendChild(bikeTable);
-  const bikeRow = document.createElement("tr");
-  bikeTable.appendChild(bikeRow);
-  const bikeHeader1 = document.createElement("th");
-  bikeTable.appendChild(bikeHeader1);
-  bikeHeader1.innerHTML = " Company";
-  const bikeHeader2 = document.createElement("th");
-  bikeTable.appendChild(bikeHeader2);
-  bikeHeader2.innerHTML = " Country";
-  const bikeHeader3 = document.createElement("th");
-  bikeTable.appendChild(bikeHeader3);
-  bikeHeader3.innerHTML = " City";
+  const networkTable = document.createElement("table");
+  networkTable.setAttribute("id", "network-table");
+  networkContainer.appendChild(networkTable);
+  const networkDetailRow = document.createElement("tr");
+  networkTable.appendChild(networkDetailRow);
+  const networkHeader1 = document.createElement("th");
+  networkTable.appendChild(networkHeader1);
+  networkHeader1.innerHTML = " Company";
+  const networkHeader2 = document.createElement("th");
+  networkTable.appendChild(networkHeader2);
+  networkHeader2.innerHTML = " Country";
+  const networkHeader3 = document.createElement("th");
+  networkTable.appendChild(networkHeader3);
+  networkHeader3.innerHTML = " City";
 
-  return bikeTable;
+  return networkTable;
 }
-function generateBikeTable(responseJson) {
-  const bikeTable = generateTableHeader();
+function generatenetworkTable(responseJson) {
+  const networkTable = generateTableHeader();
   const base = 310;
-  for (let index = base; index < base + 20; index++) {
-    const bikeRow = document.createElement("tr");
-    bikeTable.appendChild(bikeRow);
-    const bikeTD1 = document.createElement("td");
-    bikeRow.appendChild(bikeTD1);
-    const bikeTD2 = document.createElement("td");
-    bikeRow.appendChild(bikeTD2);
-    const bikeTD3 = document.createElement("td");
-    bikeRow.appendChild(bikeTD3);
+  for (let index = 0; index < base + 100; index++) {
+    const networkDetailRow = document.createElement("tr");
+    networkTable.appendChild(networkDetailRow);
+    const networkTD1 = document.createElement("td");
+    networkRow.appendChild(networkTD1);
+    const networkTD2 = document.createElement("td");
+    networkRow.appendChild(networkTD2);
+    const networkTD3 = document.createElement("td");
+    networkRow.appendChild(networkTD3);
 
     let company = responseJson.networks[index].company;
     let city = responseJson.networks[index].location.city;
     let country = responseJson.networks[index].location.country;
 
-    // console.log(responseJson);
-    // console.log(company);
-    // console.log(city);
-    // console.log(country);
+    networkTD1.innerHTML = " " + company;
+    networkTD2.innerHTML = " " + country;
+    networkTD3.innerHTML = " " + city;
+  }
+}
 
-    //TODO fix spacing with padding
+function generateNetworkDetailTable(responseJson) {
+  const networkSectionTable = generateNetworkDetailTableHeader();
+  const base = 215;
+  for (let index = base; index < base + 10; index++) {
+    const networkDetailRow = document.createElement("tr");
+    networkSectionTable.appendChild(networkDetailRow);
+    const networkDetailTD1 = document.createElement("td");
+    networkDetailRow.appendChild(networkDetailTD1);
+    const networkDetailTD2 = document.createElement("td");
+    networkDetailRow.appendChild(networkDetailTD2);
+    const networkDetailTD3 = document.createElement("td");
+    networkDetailRow.appendChild(networkDetailTD3);
 
-    bikeTD1.innerHTML = " " + company;
-    bikeTD2.innerHTML = " " + country;
-    bikeTD3.innerHTML = " " + city;
+    let name = responseJson.network.stations[index].name;
+    let emptySlots = responseJson.network.stations[index].empty_slots;
+    let eBikes = responseJson.network.stations[index].extra.eBikes;
+
+    networkDetailTD1.innerHTML = " " + name;
+    networkDetailTD2.innerHTML = " " + emptySlots;
+    networkDetailTD3.innerHTML = " " + eBikes;
   }
 }
