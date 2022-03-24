0e549c44a9eef83297915cef1c1959657c6d3dfc
Nell B <nellb28@yahoo.com>
2022-03-24 18:58:26 -0500

    More valid updates

---
+++ api-key.js
@@ -0,0 +1,5 @@
+const UT_API_KEY =
+  "snPRrN2pjpFCbg2mJ-U53MBUyKhFcUcpedytlXHMrjwYWyahufKkRd2gKX2cADUZR8Q";
+const GOOGLE_API_KEY = "AIzaSyAxYnmCQR38hXxCyBQs0T-TKSlQ7eSEpwU";
+//Google sample request
+//src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAxYnmCQR38hXxCyBQs0T-TKSlQ7eSEpwU&callback=initMap"
